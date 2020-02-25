b6f58f434262c94b651e0c2d3498cfd86ab84b4c
tombetthauser <tombetthauser@gmail.com>
2020-02-24 19:53:09 -0800

    Add User model

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-// import "./app.css";
 
-app.get("/", (req, res) => res.send("<div style='background-color: pink'>Whassup World?</div><div class='erinsDiv'>Hi Erin!</div>"));
-
-app.listen(5000, () => console.log("Server running on port 5000"))
+app.get("/", (req, res) => res.send("<div style='background-color: pink; width: 50px; height: 50px; cursor: pointer;'></div><div style='background-color: cornflowerblue; width: 50px; height: 50px; cursor: pointer;'></div>"));
+app.listen(5000, () => console.log("Server running on port 5000"));
