119a2af5dd51a28ceb1d395aee0427d83fc94dde
tombetthauser <tombetthauser@gmail.com>
2020-02-24 21:08:24 -0800

    Set up nodemon

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<div style='background-color: pink; width: 50px; height: 50px; cursor: pointer;'></div><div style='background-color: cornflowerblue; width: 50px; height: 50px; cursor: pointer;'></div>"));
+app.get("/", (req, res) => res.send("<div style='display: inline-block; background-color: pink; width: 50px; height: 50px; cursor: pointer;'></div><div style='display: inline-block; background-color: cornflowerblue; width: 50px; height: 50px; cursor: pointer;'></div>"));
+app.use(bodyParser.json());
+
 app.listen(5000, () => console.log("Server running on port 5000"));
