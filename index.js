e8e4e63a39187d0c6b96719f08591c58664e9621
tombetthauser <tombetthauser@gmail.com>
2020-02-24 21:41:24 -0800

    Connect to mongo successfully

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("<div style='display: inline-block; background-color: pink; width: 50px; height: 50px; cursor: pointer;'></div><div style='display: inline-block; background-color: cornflowerblue; width: 50px; height: 50px; cursor: pointer;'></div>"));
 app.use(bodyParser.json());
