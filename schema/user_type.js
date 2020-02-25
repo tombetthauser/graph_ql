060b33bc5228c1e8c945683df9ba4505a904e000
tombetthauser <tombetthauser@gmail.com>
2020-02-25 11:57:48 -0800

    Resolve circular query

--- schema/user_type.js
+++ schema/user_type.js
@@ -1,13 +1,26 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLNonNull } = graphql;
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLNonNull, GraphQLList } = graphql;
+
+const mongoose = require("mongoose");
+const User = mongoose.model("user");
 
 const UserType = new GraphQLObjectType({
   name: "UserType",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    email: { type: GraphQLString }
-  }
+    email: { type: GraphQLString },
+    posts: {
+      type: new GraphQLList(require("./post_type")),
+      resolve(parentValue) {
+        return (
+          User.findById(parentValue.id)
+            .populate("posts")
+            .then(user => user.posts)
+        );
+      }
+    }
+  })
 });
 
 module.exports = UserType;
